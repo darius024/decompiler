5346d88b2414b5219d9caa36b70f87c9c61c32b5
Darius Aron <darius.aron23@imperial.ac.uk>
2025-01-21 17:01:26 +0000

    feat: :sparkles: added error configuration and lexer helpers
    [anb122] [da1823] [rm1723] [ss5123]

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -3,10 +3,11 @@ package wacc
 import parsley.Parsley
 import parsley.token.{Lexer, Basic}
 import parsley.token.descriptions.*
+import parsley.token.errors.*
 
 object lexer {
     private val escDesc = EscapeDesc.plain.copy(
-        literals = Set( '\"', '\'', '\\'),
+        literals = Set('\"', '\'', '\\'),
         mapping = Map(
             "0" -> 0x0000, 
             "b" -> 0x0008,
@@ -29,8 +30,8 @@ object lexer {
                 "begin", "end", "is", "skip", "return", "exit",
                 "read", "free", "print", "println",
                 "if", "then", "else", "fi", "while", "do", "done",
-                "newpair", "fst", "snd", "call", "null", 
-                "true", "false", 
+                "newpair", "fst", "snd", "call",
+                "true", "false", "null", 
             ),
             hardOperators = Set(
                 "+", "-", "*", "/", "%", 
@@ -53,12 +54,33 @@ object lexer {
             lineCommentStart = "#",
         ),
     )
-    private val lexer = Lexer(desc)
+
+    private val errConfig = new ErrorConfig {
+        override def labelSymbol = List(
+            List("!", "-", "len", "ord", "chr").map(_ -> Label("unary operator")),
+            List("*", "%", "/", "+", "-", ">", ">=", "<", "<=", "==", "!=", "&&", "||").map(_ -> Label("binary operator")),
+            List("read", "free", "return", "exit", "print", "println", "if", "while", "begin").map(_ -> Label("statement")),
+            List("then" -> Label("then branch")),
+            List("else" -> Label("else branch")),
+            List("do" -> Label("while body")),
+            List("fi" -> Label("end of if statement")),
+            List("done" -> Label("end of while statement")),
+            List("end" -> Label("end of block")),
+        ).flatten.toMap
+    }
+        
+    private val lexer = Lexer(desc, errConfig)
 
     val identifier = lexer.lexeme.names.identifier
-    val integer = lexer.lexeme.integer.decimal32
+    val integer = lexer.lexeme.integer.decimal
     val character = lexer.lexeme.character.ascii
     val string = lexer.lexeme.string.ascii
+
+    // Lexeme combinators
+    def brackets[A](p: => Parsley[A]): Parsley[A] = lexer.lexeme.brackets(p)
+    def parens[A](p: => Parsley[A]): Parsley[A] = lexer.lexeme.parens(p)
+    def commaSep[A](p: Parsley[A]): Parsley[List[A]] = lexer.lexeme.commaSep(p)
+    def semiSep1[A](p: Parsley[A]): Parsley[List[A]] = lexer.lexeme.semiSep1(p)
 
     val implicits = lexer.lexeme.symbol.implicits
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
