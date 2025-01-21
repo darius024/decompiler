ae68a6189d52b2b9d3b84cb60332f0681188c491
Darius Aron <darius.aron23@imperial.ac.uk>
2025-01-21 21:08:04 +0000

    feat: :sparkles: introduced companion objects that extend bridge traits
    [anb122] [da1823] [rm1723] [ss5123]

--- src/main/wacc/syntax/expr.scala
+++ src/main/wacc/syntax/expr.scala
@@ -1,5 +1,7 @@
 package wacc.syntax
 
+import parsley.generic.*
+
 object expr {
     sealed trait Expr extends RValue
     case class Or(lhs: ExprAnd, rhs: Expr) extends Expr
@@ -38,7 +40,7 @@ object expr {
     case class BoolLit(value: Boolean) extends Atom
     case class CharLit(value: Char) extends Atom
     case class StrLit(value: String) extends Atom
-    case object PairLit extends Atom
+    case object PairLit extends Atom with ParserBridge0[Atom]
     case class Id(value: String) extends Atom with LValue
     case class ArrayElem(id: Id, indices: List[Expr]) extends Atom with LValue
     case class ParensExpr(expr: Expr) extends Atom
@@ -52,4 +54,43 @@ object expr {
     sealed trait PairElem extends LValue with RValue
     case class Fst(value: LValue) extends PairElem
     case class Snd(value: LValue) extends PairElem
+
+
+    object Or extends ParserBridge2[ExprAnd, Expr, Expr]
+    object And extends ParserBridge2[ExprEq, ExprAnd, ExprAnd]
+
+    object Equal extends ParserBridge2[ExprIneq, ExprIneq, ExprEq]
+    object NotEqual extends ParserBridge2[ExprIneq, ExprIneq, ExprEq]
+
+    object Greater extends ParserBridge2[ExprAdd, ExprAdd, ExprIneq]
+    object GreaterEqual extends ParserBridge2[ExprAdd, ExprAdd, ExprIneq]
+    object Less extends ParserBridge2[ExprAdd, ExprAdd, ExprIneq]
+    object LessThan extends ParserBridge2[ExprAdd, ExprAdd, ExprIneq]
+
+    object Add extends ParserBridge2[ExprAdd, ExprMul, ExprAdd]
+    object Sub extends ParserBridge2[ExprAdd, ExprMul, ExprAdd]
+
+    object Mul extends ParserBridge2[ExprMul, ExprUnary, ExprMul]
+    object Div extends ParserBridge2[ExprMul, ExprUnary, ExprMul]
+
+    object Not extends ParserBridge1[ExprUnary, ExprUnary]
+    object Neg extends ParserBridge1[ExprUnary, ExprUnary]
+    object Len extends ParserBridge1[ExprUnary, ExprUnary]
+    object Ord extends ParserBridge1[ExprUnary, ExprUnary]
+    object Chr extends ParserBridge1[ExprUnary, ExprUnary]
+
+    object IntLit extends ParserBridge1[Int, IntLit]
+    object BoolLit extends ParserBridge1[Boolean, BoolLit]
+    object CharLit extends ParserBridge1[Char, CharLit]
+    object StrLit extends ParserBridge1[String, StrLit]
+    object Id extends ParserBridge1[String, Id]
+    object ArrayElem extends ParserBridge2[Id, List[Expr], ArrayElem]
+    object ParensExpr extends ParserBridge1[Expr, ParensExpr]
+
+    object ArrayLit extends ParserBridge1[List[Expr], ArrayLit]
+    object NewPair extends ParserBridge2[Expr, Expr, NewPair]
+    object Call extends ParserBridge2[Id, List[Expr], Call]
+
+    object Fst extends ParserBridge1[LValue, Fst]
+    object Snd extends ParserBridge1[LValue, Snd]
 }
