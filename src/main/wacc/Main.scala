f31ba865d8a42e13076e134d4a8a981dc710a9c2
Adithya Narayanan <adithya.balaji22@imperial.ac.uk>
2025-03-04 17:53:02 +0000

    Revert "refactor: :art: reduced leakage of I/O to internal functionality"
    This reverts commit c27cbb0b0e34df315a461108814446c8f2cf5a39

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -5,48 +5,6 @@ import parsley.{Success, Failure}
 import wacc.backend.*
 import wacc.frontend.*
 import wacc.backend.formatter.SyntaxStyle
-import java.io.OutputStream
-
-// compilation pipeline
-def compile(file: File): (String, ExitCode) = {
-    // parsing and syntax analysis
-    val ast = parser.parse(file) match {
-        case Success(ast) => ast
-        case Failure(err) => return (s"${err.message}", ExitCode.SyntaxErr)
-    }
-
-    // semantic analysis: scope and type checking
-    val tyAst = semantics.check(ast) match {
-        case Right(tyAst) => tyAst
-        case Left(errs) => return (s"${semantics.format(errs, file)}", ExitCode.SemanticErr)
-    }
-
-    // code generation
-    val gen = generator.generate(tyAst)
-
-    // open output file: {prog}.wacc --> {prog}.s
-    given out: OutputStream = os.write.outputStream(
-        os.pwd / s"${file.getName.stripSuffix(".wacc")}.s"
-    )
-
-    // asm formatting
-    try {
-        formatter.format(gen, SyntaxStyle.Intel)
-    } finally {
-        out.close()
-    }
-
-    ("Code compiled successfully.", ExitCode.Success)
-}
-
-/** Using an enum of fixed codes to prevent invalid codes being used. */
-enum ExitCode(val code: Int) {
-    case Success     extends ExitCode(0)
-    case SyntaxErr   extends ExitCode(100)
-    case SemanticErr extends ExitCode(200)
-
-    def enforce(): Unit = System.exit(code)
-}
 
 // entry point
 @main
@@ -54,4 +12,43 @@ def main(path: String): Unit = {
     val (errs, code) = compile(new File(path))
     println(errs)
     code.enforce()
+}
+
+// compilation pipeline
+def compile(file: File): (String, ExitCode) = {
+    // parsing and syntax analysis
+    val ast = parser.parse(file) match {
+        // on successful compilation, the AST is returned
+        case Success(ast) => ast
+        // otherwise, there is a syntax error
+        case Failure(err) => return (s"${err.message}", ExitCode.SyntaxErr)
+    }
+
+    // semantic analysis: scope and type checking
+    val typedAst = semantics.check(ast) match {
+        // on successful analysis, the typed AST is returned
+        case Right(tyAst) => tyAst
+        // otherwise, there is a semantic error
+        case Left(errs) => return (s"${semantics.format(errs, file)}", ExitCode.SemanticErr)
+    }
+
+    // code generation and assembly formatter
+    formatter.format(generator.generate(typedAst), file, SyntaxStyle.Intel)
+
+    ("Code compiled successfully.", ExitCode.Success)
+}
+
+/** Using an enum of fixed codes to prevent invalid codes being used. */
+enum ExitCode(val code: Int) {
+    case Success     extends ExitCode(exitCodes.SUCCESS)
+    case SyntaxErr   extends ExitCode(exitCodes.SYNTAX_ERROR)
+    case SemanticErr extends ExitCode(exitCodes.SEMANTIC_ERROR)
+
+    def enforce(): Unit = System.exit(code)
+}
+
+object exitCodes {
+    final val SUCCESS = 0
+    final val SYNTAX_ERROR = 100
+    final val SEMANTIC_ERROR = 200
 }
